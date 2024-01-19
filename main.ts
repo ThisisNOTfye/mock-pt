5b3c6bb5492732bb02cdc18abb9332c99c983871
ThisisNOTfye <156244854+ThisisNOTfye@users.noreply.github.com>
2024-01-19 12:18:32 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,5 @@
 let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level0`)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(37, 212))
