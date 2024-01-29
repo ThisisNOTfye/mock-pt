55ccfa58ca5993c1260dd0989a41d4ce53873f79
ThisisNOTfye <156244854+ThisisNOTfye@users.noreply.github.com>
2024-01-29 14:50:37 -0600

    AVERAGE OF CAMERA TO MIDDLE THE SCREEN

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-	
-})
+function monaco_map () {
+    tiles.setCurrentTilemap(tilemap`level4`)
+}
 function movement () {
-    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 200, 200)
+    controller.player1.moveSprite(p1, 200, 200)
+    controller.player3.moveSprite(p3, 200, 200)
+    controller.player2.moveSprite(p2, 200, 200)
+    controller.player4.moveSprite(p4, 200, 200)
 }
 function spawn_point () {
     tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(27, 219))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), tiles.getTileLocation(28, 219))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)), tiles.getTileLocation(29, 219))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)), tiles.getTileLocation(30, 219))
 }
 function spawn_point_nurburgring () {
     tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(37, 213))
@@ -19,9 +25,20 @@ function nuburgring_map () {
 function tile_map () {
     tiles.setCurrentTilemap(tilemap`level`)
 }
+function spawn_point2 () {
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(8, 94))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), tiles.getTileLocation(9, 96))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)), tiles.getTileLocation(10, 98))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)), tiles.getTileLocation(11, 100))
+}
 function camera_movement () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
 }
+let camera_sprite: Sprite = null
+let p4: Sprite = null
+let p3: Sprite = null
+let p2: Sprite = null
+let p1: Sprite = null
 let myMenu = miniMenu.createMenu(
 miniMenu.createMenuItem("The Nurburgring"),
 miniMenu.createMenuItem("Autodromo nazionale"),
@@ -34,6 +51,29 @@ myMenu.onButtonPressed(controller.A, function (selection, selectedIndex) {
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`myImage1`, SpriteKind.Player))
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`myImage2`, SpriteKind.Player))
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`myImage0`, SpriteKind.Player))
+    p1 = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    p2 = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+    p3 = sprites.create(assets.image`myImage2`, SpriteKind.Player)
+    p4 = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+    camera_sprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b . . . . . . . . 
+        . . . . . . b b . . . . . . . . 
+        . . . . . . b b . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    scene.cameraFollowSprite(camera_sprite)
     if (selectedIndex == 0) {
         camera_movement()
         nuburgring_map()
@@ -47,6 +87,12 @@ myMenu.onButtonPressed(controller.A, function (selection, selectedIndex) {
         movement()
     }
     if (selectedIndex == 2) {
-    	
+        camera_movement()
+        monaco_map()
+        spawn_point2()
+        movement()
     }
+})
+game.onUpdate(function () {
+    camera_sprite.setPosition((p2.x + (p3.x + (p1.x + p4.x))) / 4, (p2.y + (p3.y + (p1.y + p4.y))) / 4)
 })
